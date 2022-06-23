bd5cf5af77bf03fb9e0fee5a3f669883d85a6746
Tal <x@mail.com>
2022-06-23 16:25:47 +0300

    interview question

--- src/main/java/interviewQuestions/playList/Song.java
+++ src/main/java/interviewQuestions/playList/Song.java
@@ -5,13 +5,11 @@ public class Song {
     private final String artist;
 
     public Song(String songName, String artist) {
-
         this.songName = songName;
         this.artist = artist;
     }
 
     public void printSong() {
-
         System.out.println("[Name: " + this.songName + "| Artist: " + this.artist + "]");
     }
 }
