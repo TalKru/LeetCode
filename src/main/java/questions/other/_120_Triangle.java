3a8096687f7d360e552e8b37dcc7532bfcb2aa30
TalKru <tal.krutoy@gmail.com>
2022-07-06 16:50:33 +0300

    update solutions

--- src/main/java/questions/other/_120_Triangle.java
+++ src/main/java/questions/other/_120_Triangle.java
@@ -1,5 +1,6 @@
 package questions.other;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -32,27 +33,28 @@ triangle[i].length == triangle[i - 1].length + 1
 public class _120_Triangle {
 
     public static void main(String[] args) {
-//        List<List<Integer>> triangle = List.of(
-//                List.of(2),
-//                List.of(3, 4),
-//                List.of(6, 5, 7),
-//                List.of(4, 1, 8, 3)
-//        );
-        List<List<Integer>> triangle = List.of(
-                List.of(-1),
-                List.of(9, -2),
-                List.of(0, 4, 5),
-                List.of(7, 4, -4, -5),
-                List.of(9, 6, 0, 5, 7),
-                List.of(9, 2, -9, -4, 5, -2),
-                List.of(-4, -9, -5, -7, -5, -5, -2),
-                List.of(-9, 5, -6, -4, 4, 1, 6, -4),
-                List.of(-4, 3, 9, -2, 8, 6, -9, -2, -2),
-                List.of(7, -6, 9, 8, -4, 2, -4, -2, -1, -2),
-                List.of(0, 3, 2, 4, 0, -6, 7, 6, 7, -5, 2),
-                List.of(9, 0, -8, 6, 4, 6, 2, 5, -9, 9, -1, -6),
-                List.of(6, -3, -4, -5, 0, 3, 3, 4, -6, -4, -7, 7, 3)
+
+        List<List<Integer>> triangle = Arrays.asList(
+                Arrays.asList(2),
+                Arrays.asList(3, 4),
+                Arrays.asList(6, 5, 7),
+                Arrays.asList(4, 1, 8, 3)
         );
+//        List<List<Integer>> triangle = List.of(
+//                List.of(-1),
+//                List.of(9, -2),
+//                List.of(0, 4, 5),
+//                List.of(7, 4, -4, -5),
+//                List.of(9, 6, 0, 5, 7),
+//                List.of(9, 2, -9, -4, 5, -2),
+//                List.of(-4, -9, -5, -7, -5, -5, -2),
+//                List.of(-9, 5, -6, -4, 4, 1, 6, -4),
+//                List.of(-4, 3, 9, -2, 8, 6, -9, -2, -2),
+//                List.of(7, -6, 9, 8, -4, 2, -4, -2, -1, -2),
+//                List.of(0, 3, 2, 4, 0, -6, 7, 6, 7, -5, 2),
+//                List.of(9, 0, -8, 6, 4, 6, 2, 5, -9, 9, -1, -6),
+//                List.of(6, -3, -4, -5, 0, 3, 3, 4, -6, -4, -7, 7, 3)
+//        );
         System.out.println("Input triangle:");
 
         for (List<Integer> list : triangle) {
@@ -70,8 +72,31 @@ public class _120_Triangle {
 
         System.out.println("Min path from top to bottom (sum): " + minSum);
     }
+    // ============================================================================================== //
 
+    // In place, bottom up
     public static int minimumTotal(List<List<Integer>> triangle) {
+
+        int LEVELS = triangle.size();
+
+        for (int level = LEVELS - 2; level >= 0 ; level--) { // going up the triangle for the prev to last level
+
+            int levelLen = triangle.get(level).size(); // [0][1]...[levelLen - 2][levelLen - 1]
+
+            for (int i = 0; i < levelLen; i++) {
+
+                int leftChild  = triangle.get(level + 1).get(i);
+                int rightChild = triangle.get(level + 1).get(i + 1);
+                int minChild = Math.min(leftChild, rightChild);
+                int addMinChildVal = triangle.get(level).get(i) + minChild; // each node adds to itself the min value from one of its children
+
+                triangle.get(level).set(i, addMinChildVal); // update the node itself
+            }
+        }
+        return triangle.get(0).get(0);
+    }
+    // ============================================================================================== //
+    public static int minimumTotal2(List<List<Integer>> triangle) {
 
         if (triangle == null || triangle.size() == 0) {
             return 0;
@@ -109,5 +134,5 @@ public class _120_Triangle {
         return min;
         //return Arrays.stream(pathsArr).min().getAsInt();
     }
-
+    // ============================================================================================== //
 }
