dbaa7540948d482dffb41939c0a5d83a95beb53c
Tal <x@mail.com>
2022-07-07 19:26:42 +0300

    blank

---
+++ src/main/java/questions/strings/_1002_Find_Common_Characters.java
@@ -0,0 +1,44 @@
+package questions.strings;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+Given a string array words,
+return an array of all characters that show up in all strings
+within the words (including duplicates).
+You may return the answer in any order.
+
+Example 1:
+Input: words = ["bella","label","roller"]
+Output: ["e","l","l"]
+
+Example 2:
+Input: words = ["cool","lock","cook"]
+Output: ["c","o"]
+
+Constraints:
+1 <= words.length <= 100
+1 <= words[i].length <= 100
+words[i] consists of lowercase English letters.
+ */
+public class _1002_Find_Common_Characters {
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+    public static List<String> commonChars(String[] words) {
+
+        List<String> list = new ArrayList<>();
+
+
+
+
+
+
+        return list;
+    }
+}
