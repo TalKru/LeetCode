583d472cdec2dba6999d25d1007adb462477bc70
TalKru <tal.krutoy@gmail.com>
2022-07-11 12:59:06 +0300

    solution

--- src/main/java/questions/strings/_1002_Find_Common_Characters.java
+++ src/main/java/questions/strings/_1002_Find_Common_Characters.java
@@ -1,7 +1,6 @@
 package questions.strings;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 /*
 Given a string array words,
@@ -26,19 +25,66 @@ public class _1002_Find_Common_Characters {
 
     public static void main(String[] args) {
 
+        String[] words = {"bella","label","roller"};
+        List<String> commonCharacters = commonChars(words);
 
-
+        System.out.println(commonCharacters);
     }
 
     public static List<String> commonChars(String[] words) {
 
+        if (words.length == 1) {
+            //return List.of(words[0]);
+            return Arrays.asList(words[0]);
+        }
+        String tempWord = listToSrting(commonChars(words[0], words[1]));
+
+        for (int i = 2; i < words.length; i++) { // [i = 2], 0,1 handled outside
+
+            tempWord = listToSrting(commonChars(words[i], tempWord));
+        }
         List<String> list = new ArrayList<>();
 
-
-
-
-
-
+        for (int i = 0; i < tempWord.length(); i++) {
+            list.add(String.valueOf(tempWord.charAt(i)));
+        }
         return list;
     }
+
+    public static List<Character> commonChars(String s1, String s2) { // overloading
+
+        List<Character> chars = new LinkedList<>();
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < s1.length(); i++) {
+
+            char c = s1.charAt(i);
+
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+        for (int i = 0; i < s2.length(); i++) {
+
+            char c = s2.charAt(i);
+
+            if (map.containsKey(c) && map.get(c) > 0) {
+
+                chars.add(c);
+
+                map.put(c, map.get(c) - 1);
+            }
+        }
+        return chars;
+    }
+
+    private static String listToSrting(List<Character> charList) {
+
+        String str = "";
+
+        for (Character c : charList) {
+
+            str += c;
+        }
+        return str;
+    }
+
 }
