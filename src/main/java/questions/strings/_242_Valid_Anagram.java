677346a3e3651be54baf321df63a074b2227b088
Tal <x@mail.com>
2022-07-28 18:56:37 +0300

    improvement

--- src/main/java/questions/strings/_242_Valid_Anagram.java
+++ src/main/java/questions/strings/_242_Valid_Anagram.java
@@ -1,6 +1,7 @@
 package questions.strings;
 
 import java.util.Arrays;
+import java.util.HashMap;
 
 /*
 Given two strings s and t, return true if t is an anagram of s,
@@ -29,17 +30,44 @@ public class _242_Valid_Anagram {
 		String s2 = "nagaram";
 		boolean res = isAnagram(s1, s2);
 		
-		System.out.println("s1: " + s1);
-		System.out.println("s2: " + s2);
-		System.out.println("Is s1 is an anagram od s2: " + res);
+		System.out.println("string 1: " + s1);
+		System.out.println("string 2: " + s2);
+		System.out.println("Is string 1 is an anagram of string 2: " + res);
 	}
 
 
-	// 2*O(nlog(n)) + 3 * O(n) = O(nlog(n))
 	public static boolean isAnagram(String s, String t) {
+
+		if(s.length() != t.length()) {
+			return false;
+		}
+		HashMap<Character, Integer> map = new HashMap<>();
+
+		for (int i = 0; i < s.length(); i++) { // build mapping of the (Characters , count)
+
+			char c = s.charAt(i);
+
+			map.put(c, map.getOrDefault(c, 0) + 1);
+		}
+		for (int i = 0; i < t.length(); i++) {
+
+			char c = t.charAt(i);
+			Integer count = map.get(c);
+
+			if (count == null || count == 0) {
+				return false;
+			}
+			map.put(c, count - 1);
+		}
+		return true;
+	}
+
+	//  3 * O(n) + 2 * O(nlog(n)) = O(nlog(n))
+	public static boolean isAnagram2(String s, String t) {
 		
-		if(s.length() != t.length()) return false;
-		
+		if(s.length() != t.length()) {
+			return false;
+		}
 		char[] charArr1 = s.toCharArray();
 		char[] charArr2 = t.toCharArray();
 		
@@ -48,6 +76,7 @@ public class _242_Valid_Anagram {
 		
 		return Arrays.equals(charArr1, charArr2);
 	}
+
 }
 
 
