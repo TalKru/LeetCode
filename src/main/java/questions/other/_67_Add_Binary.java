ebfd0a088e8070fba1c4451e47e1dd922239f927
Tal <x@mail.com>
2022-09-01 23:29:32 +0300

    minor diff, no logic was changed

--- src/main/java/questions/other/_67_Add_Binary.java
+++ src/main/java/questions/other/_67_Add_Binary.java
@@ -22,43 +22,36 @@ public class _67_Add_Binary {
 		
 		String s1 = "1110";
 		String s2 = "1010";
-		
 		String add = addBinary(s1, s2);
-		
 		System.out.println(s1);
 		System.out.println(s2);
 		System.out.println(add);
 	}
-
 
 	static String addBinary(String a, String b) {
 		
 		if(b.equals("0")) { return a; }
 		if(a.equals("0")) { return b; }
 		
-		StringBuilder result = new StringBuilder("");  // Initialize result
+		StringBuilder result = new StringBuilder();
 
 		int s = 0; // Initialize digit sum        
 		int i = a.length() - 1;
 		int j = b.length() - 1;
 
 		while (i >= 0 || j >= 0 || s == 1) {
-
 			// Comput sum of last
 			// digits and carry
 			s += ((i >= 0)? a.charAt(i) - '0': 0);
 			s += ((j >= 0)? b.charAt(j) - '0': 0);
-
 			// If current digit sum is
 			// 1 or 3, add 1 to result
 			result.append((char)(s % 2 + '0'));
 
 			s /= 2; // Compute carry
-
 			i--; // Move to next digits
 			j--; 
 		}
-
 		// Remove leading zeros, if any
 		int start = result.length()-1;
 
